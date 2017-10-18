bb4899fcf848ff0e061d27174ec84944a38bead4
ryan.w.daly <ryan.w.daly90@gmail.com>
2017-10-18 08:24:19 -0700

    Rails homework, W3D3

---
+++ W3D3/demo_project/db/migrate/20171018051612_create_person.rb
@@ -0,0 +1,12 @@
+class CreatePerson < ActiveRecord::Migration[5.1]
+  def change
+    create_table :people do |t|
+
+      t.string :name, null: false
+      t.integer :house_id, null: false
+        t.timestamps
+    end
+
+
+  end
+end
